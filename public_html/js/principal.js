81fc1900c391426e869c5d0a0b6d586e0fa6816d
Camilo Rodriguez <crodriguez@usbbog.edu.co>
2013-09-06 08:56:55 -0500

    cc-js

---
+++ public_html/js/principal.js
@@ -0,0 +1,13 @@
+$(
+ function() 
+ {
+  
+   $("#boton1").on("click", miBoton);
+ }         
+   
+ );
+ function miBoton()
+ {
+    // $("#parrafo3").toggle(2000);
+    $("#parrafo3").css("background-color","blue");
+ }
